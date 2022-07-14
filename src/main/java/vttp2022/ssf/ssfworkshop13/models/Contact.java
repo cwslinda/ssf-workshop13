ae5e63b36b0d9a1b3bf7b431e1726284ff57af7f
Linda Cheung <cws@Lindas-MacBook-Pro.local>
2022-07-14 13:56:09 +0800

    added IOUtil

--- src/main/java/vttp2022/ssf/ssfworkshop13/models/Contact.java
+++ src/main/java/vttp2022/ssf/ssfworkshop13/models/Contact.java
@@ -1,16 +1,40 @@
 package vttp2022.ssf.ssfworkshop13.models;
 
+import java.util.Random;
+
 public class Contact {
     private String name;
     private String email;
     private int phoneNumber;
     private String id;
 
+    public Contact(){
+        this.id = this.generateId(8);
+    }
 
+    public Contact(String name, String email, int phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String id, String name, String email, int phoneNumber){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
     // two classes calling the same method, it will be one at a time.
-    // synchronized don't allow for concurrent calls
-    private synchronized String generateId(){
+    // synchronized don't allow for concurrent calls, queuing system for generating id
+    private synchronized String generateId(int numChars){
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        while(sb.length() < numChars){
+            sb.append(Integer.toHexString(r.nextInt()));
+
+        }
        
+        return sb.toString().substring(0, numChars);
 
     }
     public String getName() {
---
+++ src/main/java/vttp2022/ssf/ssfworkshop13/util/IOUtil.java
@@ -0,0 +1,11 @@
+package vttp2022.ssf.ssfworkshop13.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IOUtil {
+    private static final Logger logger = LoggerFactory.getLogger(IOUtil.class);
+
+    
+    
+}
