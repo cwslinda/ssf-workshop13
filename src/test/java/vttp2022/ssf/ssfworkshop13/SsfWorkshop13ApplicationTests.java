bbd06a4d2e964c4907e8cc9e7893d3a598fc1ed0
Linda Cheung <cws@Lindas-MacBook-Pro.local>
2022-07-14 16:49:03 +0800

    finished

--- src/test/java/vttp2022/ssf/ssfworkshop13/SsfWorkshop13ApplicationTests.java
+++ src/test/java/vttp2022/ssf/ssfworkshop13/SsfWorkshop13ApplicationTests.java
@@ -1,7 +1,12 @@
 package vttp2022.ssf.ssfworkshop13;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import vttp2022.ssf.ssfworkshop13.models.Contact;
+
+// autowired cannot be used in testcase
 
 @SpringBootTest
 class SsfWorkshop13ApplicationTests {
@@ -10,4 +15,23 @@ class SsfWorkshop13ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testContact() throws Exception{
+		Contact c = new Contact();
+		c.setEmail("apple@gmail.com");
+		c.setName("apple");
+		c.setPhoneNumber(123123123);
+		assertEquals(c.getEmail(), "apple@gmail.com");
+	}
+
+	// @Test
+	// public void testContactsSaveContact() throws Exception{
+	// 	Contact ctc = new Contact();
+	// 	ctc.setName("Grace");
+	// 	ctc.setEmail("grace@gmail.com");
+	// 	ctc.setPhoneNumber(123123123);
+	// 	ctcz.saveContact(ctc, mdl, appArgs);
+	//}
+
 }
+
